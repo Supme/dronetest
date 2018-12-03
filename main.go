09d0df1a7ce1de63576f3258bbe7034f5b1cde31
supme <supmea@gmail.com>
2018-12-03 18:24:29 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const listenAddr = ":8080"
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	log.Printf("start webserver on %s", listenAddr)
+	panic(http.ListenAndServe(listenAddr, nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	//w.WriteHeader(http.StatusForbidden)
+	_, err := w.Write([]byte("Hello world!"))
+	if err != nil {
+		log.Print(err)
+	}
+}
